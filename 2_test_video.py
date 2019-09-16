f594dad9914ff9e3d65a1d59c9931cefccf7901e
ichsankurnia <ichsankurniawan51@gmail.com>
2019-09-16 16:50:21 +0700

    initial commit

--- 2_test_video.py
+++ 2_test_video.py
@@ -5,32 +5,52 @@ import time
 
 option = {
     'model': 'cfg/yolo.cfg',
-    'load': 'bin/yolo.weights',
-    'threshold': 0.15,
-    'gpu': 1.0
+    'load': 'bin/yolov2.weights',
+    # 'threshold': 0.15,
 }
 
 tfnet = TFNet(option)
 
-capture = cv2.VideoCapture('videofile_1080_20fps.avi')
+capture = cv2.VideoCapture('test-videos/Los Angeles Police Chase 10--11-2018 - Reckless Driver Stolen Car Pursuit.mp4')
 colors = [tuple(255 * np.random.rand(3)) for i in range(5)]
 
-while (capture.isOpened()):
+
+while 1:
     stime = time.time()
-    ret, frame = capture.read()
-    if ret:
-        results = tfnet.return_predict(frame)
-        for color, result in zip(colors, results):
-            tl = (result['topleft']['x'], result['topleft']['y'])
-            br = (result['bottomright']['x'], result['bottomright']['y'])
-            label = result['label']
-            frame = cv2.rectangle(frame, tl, br, color, 7)
-            frame = cv2.putText(frame, label, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
-        cv2.imshow('frame', frame)
-        print('FPS {:.1f}'.format(1 / (time.time() - stime)))
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
-        capture.release()
-        cv2.destroyAllWindows()
-        break
+    _, frame = capture.read()
+
+    results = tfnet.return_predict(frame)
+
+    for result in results:
+
+
+    for color, result in zip(colors, results):
+        tl = (result['topleft']['x'], result['topleft']['y'])
+        br = (result['bottomright']['x'], result['bottomright']['y'])
+        label = result['label']
+        frame = cv2.rectangle(frame, tl, br, color, 7)
+        frame = cv2.putText(frame, label, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+
+    cv2.imshow('frame', frame)
+    print('FPS {:.1f}'.format(1 / (time.time() - stime)))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+# while (capture.isOpened()):
+#     stime = time.time()
+#     ret, frame = capture.read()
+#     if ret:
+#         results = tfnet.return_predict(frame)
+#         for color, result in zip(colors, results):
+#             tl = (result['topleft']['x'], result['topleft']['y'])
+#             br = (result['bottomright']['x'], result['bottomright']['y'])
+#             label = result['label']
+#             frame = cv2.rectangle(frame, tl, br, color, 7)
+#             frame = cv2.putText(frame, label, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+#         cv2.imshow('frame', frame)
+#         print('FPS {:.1f}'.format(1 / (time.time() - stime)))
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#     else:
+#         capture.release()
+#         cv2.destroyAllWindows()
+#         break
